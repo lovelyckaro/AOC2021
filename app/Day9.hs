092a54a3470c2343958317f6d0e330ad722eba02
Love Lyckaro <love.lyckaro@hotmail.se>
2021-12-21 14:51:54 +0100

    woops, format day 9 as well

--- app/Day9.hs
+++ app/Day9.hs
@@ -1,40 +1,45 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
-import SantaLib
-import Data.Vector (Vector, (!?), (!))
-import qualified Data.Vector as V
+
+import Data.List
 import Data.Set (Set)
 import qualified Data.Set as S
-import Data.List
+import Data.Vector (Vector, (!), (!?))
+import qualified Data.Vector as V
+import SantaLib
 
 pInp :: String -> Vector (Vector Int)
-pInp = V.fromList . map (V.fromList . map (read . (:[]))) . lines
+pInp = V.fromList . map (V.fromList . map (read . (: []))) . lines
 
 isLowPoint :: Vector (Vector Int) -> (Int, Int) -> Bool
 isLowPoint vs (row, col) = length (filter (> this) neighbors) == neighborAmount
   where
     this = vs ! row ! col
-    neighbors = [vs ! r ! c | (r,c) <- possibleNeighbors, checkBounds vs (r,c)]
+    neighbors = [vs ! r ! c | (r, c) <- possibleNeighbors, checkBounds vs (r, c)]
     possibleNeighbors = [(row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
     neighborAmount = length neighbors
 
 checkBounds :: Vector (Vector a) -> (Int, Int) -> Bool
-checkBounds matrix (row, col) = row >= 0 && col >= 0 && col < width && row < height 
-  where (height, width) = (length matrix, length (V.head matrix))
+checkBounds matrix (row, col) = row >= 0 && col >= 0 && col < width && row < height
+  where
+    (height, width) = (length matrix, length (V.head matrix))
 
 determineBasin :: Vector (Vector Int) -> (Int, Int) -> [(Int, Int)]
 determineBasin matrix = connected neighbors
   where
     neighbors (row, col) = filter valid [(row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
-      where valid (r,c) = checkBounds matrix (r,c)
-                && matrix ! r ! c /= 9
-                && matrix ! r ! c > matrix ! row ! col
+      where
+        valid (r, c) =
+          checkBounds matrix (r, c)
+            && matrix ! r ! c /= 9
+            && matrix ! r ! c > matrix ! row ! col
 
 badness :: Vector (Vector Int) -> (Int, Int) -> Int
-badness  vs (row, col) = vs ! row ! col + 1
+badness vs (row, col) = vs ! row ! col + 1
 
-points :: Vector (Vector Int) -> [(Int,Int)]
-points vs = [(row, col) | row <- [0.. length vs - 1], col <- [0.. length (V.head vs) - 1]]
+points :: Vector (Vector Int) -> [(Int, Int)]
+points vs = [(row, col) | row <- [0 .. length vs - 1], col <- [0 .. length (V.head vs) - 1]]
 
 part1 :: String -> Int
 part1 s = sum badnesses
@@ -57,5 +62,5 @@ main :: IO ()
 main = do
   inp <- getInput 9
   ex <- getExample 9
-  putAnswer 9 Part1 (part1 inp) 
+  putAnswer 9 Part1 (part1 inp)
   putAnswer 9 Part2 (part2 inp)
